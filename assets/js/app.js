dd0e8fd6f2e2321517fed32005d36cbb0c8fb259
Lapops Xennias <augustineokpala2@mail.com>
2026-02-06 04:42:34 +0100

    updated the navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -211,3 +211,15 @@ document.addEventListener("DOMContentLoaded", () => {
   initContactPage();
   initHeroCarousel();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  if (hamburger && navLinks) {
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  }
+});
+
